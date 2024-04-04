0564e55e73f9af4607f87bc5ea79bfe25a2110a8
katsuyoshi ozaki <ozaki@datasign.jp>
2024-04-04 14:01:22 +0900

    Added CRL issuance function.

--- common/src/utils/datetime.ts
+++ common/src/utils/datetime.ts
@@ -18,3 +18,14 @@ export function formatDateTimeForDisplay(date: Date): string {
 
   return year + month + day + hours + minutes + seconds + "Z";
 }
+
+export function formatDateToCustomCompactForm(date: Date): string {
+  const year = date.getUTCFullYear().toString().padStart(4, "0");
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const day = date.getUTCDate().toString().padStart(2, "0");
+  const hours = date.getUTCHours().toString().padStart(2, "0");
+  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+  const seconds = date.getUTCSeconds().toString().padStart(2, "0");
+
+  return `${year}${month}${day}${hours}${minutes}${seconds}Z`;
+}
