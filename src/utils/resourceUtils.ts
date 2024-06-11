92938df60515260d37164d522ab3bba4d3098285
katsuyoshi ozaki <ozaki@datasign.jp>
2024-06-11 13:59:22 +0900

    Fixed to cache metadata in memory.

--- src/utils/resourceUtils.ts
+++ src/utils/resourceUtils.ts
@@ -1,11 +1,16 @@
 import path from "path";
 import fs from "fs/promises";
 
+let cachedMetadata: string | undefined;
+
 export const readLocalMetadataResource = async (
   dirname: string,
   filename: string,
 ) => {
-  const metadataPath = path.join(dirname, filename);
-  const fileContents = await fs.readFile(metadataPath, "utf-8");
-  return JSON.parse(fileContents);
+  if (!cachedMetadata) {
+    const metadataPath = path.join(dirname, filename);
+    const fileContents = await fs.readFile(metadataPath, "utf-8");
+    cachedMetadata = JSON.parse(fileContents);
+  }
+  return cachedMetadata
 };
