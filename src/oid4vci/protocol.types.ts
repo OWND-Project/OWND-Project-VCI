1f4824a3895bf6aa09899cd16d6d499770008881
katsuyoshi ozaki <ozaki@datasign.jp>
2024-06-13 15:25:18 +0900

    Define data types related to IssuerMetadata

--- src/oid4vci/protocol.types.ts
+++ src/oid4vci/protocol.types.ts
@@ -22,3 +22,159 @@ export interface TokenResponse {
   c_nonce?: string;
   c_nonce_expires_in?: number;
 }
+
+export interface CredentialResponseEncryption {
+  alg_values_supported: string[];
+  enc_values_supported: string[];
+  encryption_required: boolean;
+}
+
+export interface AbstractDisplay {
+  name?: string;
+  locale?: string;
+}
+
+export interface IssuerDisplay extends AbstractDisplay {
+  logo?: {
+    uri: string;
+    alt_text?: string;
+  };
+}
+
+export interface CredentialDisplay extends AbstractDisplay {
+  logo?: {
+    uri: string;
+    alt_text?: string;
+  };
+  description?: string;
+  background_color?: string;
+  background_image?: {
+    uri: string;
+  };
+  text_color?: string;
+}
+
+export interface ClaimDisplay extends AbstractDisplay {}
+
+interface BaseIssuerMetadata {
+  credential_issuer: string;
+  authorization_servers?: string[];
+  credential_endpoint: string;
+  batch_credential_endpoint?: string;
+  deferred_credential_endpoint?: string;
+  notification_endpoint?: string;
+  credential_response_encryption?: CredentialResponseEncryption;
+  credential_identifiers_supported?: boolean;
+  signed_metadata?: string;
+  display?: IssuerDisplay[];
+
+  // Add `credential_configurations_supported` depending on the type of credential
+}
+
+// A.1.1  VC Signed as a JWT, Not Using JSON-LD
+export interface IssuerMetadataJwtVcWithoutJsonLd extends BaseIssuerMetadata {
+  credential_configurations_supported: {
+    [key: string]: {
+      format: string;
+      scope?: string;
+      cryptographic_binding_methods_supported?: string[];
+      credential_signing_alg_values_supported?: string[];
+      proof_types_supported?: {
+        [key: string]: {
+          proof_signing_alg_values_supported: string[];
+        };
+      };
+      display?: CredentialDisplay[];
+
+      // Added parameters specific to A.1.1.
+      credential_definition: {
+        type: string[];
+        credentialSubject?: {
+          // todo: support nested structure
+          [key: string]: {
+            mandatory?: boolean;
+            value_type?: string;
+            display?: ClaimDisplay[];
+          };
+        };
+      };
+      order: string[];
+    };
+  };
+}
+
+// A.1.2  VC Secured using Data Integrity, using JSON-LD, with a Proof Suite Requiring Linked Data Canonicalization
+export interface IssuerMetadataDataIntegrityVcWithJsonLd
+  extends BaseIssuerMetadata {
+  credential_configurations_supported: {
+    [key: string]: {
+      format: string;
+      scope?: string;
+      cryptographic_binding_methods_supported?: string[];
+      credential_signing_alg_values_supported?: string[];
+      proof_types_supported?: {
+        [key: string]: {
+          proof_signing_alg_values_supported: string[];
+        };
+      };
+      display?: CredentialDisplay[];
+
+      // Added parameters specific to A.1.2.
+      credential_definition: {
+        "@context": string[];
+        type: string[];
+        credentialSubject?: {
+          // todo: support nested structure
+          [key: string]: {
+            mandatory?: boolean;
+            value_type?: string;
+            display?: ClaimDisplay[];
+          };
+        };
+      };
+      order: string[];
+    };
+  };
+}
+
+// A.1.3  VC signed as a JWT, Using JSON-LD
+// The definitions in Appendix A.1.2.2 apply for metadata of Credentials of this type as well.
+export interface IssuerMetadataJwtVcWithJsonLd
+  extends IssuerMetadataDataIntegrityVcWithJsonLd {}
+
+// A.3 IETF SD-JWT VC
+export interface IssuerMetadataSelectiveDisclosureJwtVc
+  extends BaseIssuerMetadata {
+  credential_configurations_supported: {
+    [key: string]: {
+      format: string;
+      scope?: string;
+      cryptographic_binding_methods_supported?: string[];
+      credential_signing_alg_values_supported?: string[];
+      proof_types_supported?: {
+        [key: string]: {
+          proof_signing_alg_values_supported: string[];
+        };
+      };
+      display?: CredentialDisplay[];
+
+      // Added parameters specific to A.3.
+      vct: string;
+      claims: {
+        // todo: support nested structure
+        [key: string]: {
+          mandatory?: boolean;
+          value_type?: string;
+          display?: ClaimDisplay[];
+        };
+      };
+      order: string[];
+    };
+  };
+}
+
+export type IssuerMetadata =
+  | IssuerMetadataJwtVcWithJsonLd
+  | IssuerMetadataJwtVcWithoutJsonLd
+  | IssuerMetadataDataIntegrityVcWithJsonLd
+  | IssuerMetadataSelectiveDisclosureJwtVc;
