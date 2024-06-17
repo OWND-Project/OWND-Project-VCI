40427da3b0c5fac67ffcec8334be82e356c7eb4c
katsuyoshi ozaki <ozaki@datasign.jp>
2024-06-17 19:33:07 +0900

    Fixes to keep up with changes in the library.

--- demos/participation-cert-vci/src/logic/credentialsConfigProvider.ts
+++ demos/participation-cert-vci/src/logic/credentialsConfigProvider.ts
@@ -1,23 +1,30 @@
 import { StoredAccessToken } from "ownd-vci/dist/store/authStore.js";
 import {
   CredentialIssuerConfig,
+  DecodedProofJwt,
   IssueJwtVcJsonCredential,
-  PayloadJwtVc,
-  ProofOfPossession,
 } from "ownd-vci/dist/oid4vci/credentialEndpoint/types.js";
 
 import { ErrorPayload, Result } from "ownd-vci/dist/types";
 import participationCertificate from "./participationCertificate.js";
 import { updateNonce } from "ownd-vci/dist/oid4vci/credentialEndpoint/defaults/nonce.js";
 import { accessTokenStateProvider } from "ownd-vci/dist/oid4vci/credentialEndpoint/defaults/accessToken.js";
+import { CredentialRequestJwtVcJson } from "ownd-vci/dist/oid4vci/types/protocol.types.js";
 
 const issueJwtJsonVcCredential: IssueJwtVcJsonCredential = async (
   preAuthorizedCode: string,
-  payload: PayloadJwtVc,
-  proofOfPossession?: ProofOfPossession,
+  payload: CredentialRequestJwtVcJson,
+  proofOfPossession?: DecodedProofJwt,
 ): Promise<Result<string, ErrorPayload>> => {
   console.log(`payload : ${JSON.stringify(payload)}`);
-  const { type } = payload.credential_definition;
+  const type = payload.credential_definition?.type;
+
+  if (!type) {
+    const error = {
+      error: "missing_credential_definition",
+    };
+    return { ok: false, error };
+  }
 
   console.debug({ payload });
 
